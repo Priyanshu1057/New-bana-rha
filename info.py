505e0f79ab33a5048cd4ed176b8fac13a3273891
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2026-04-19 00:00:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 COVERX = bool(environ.get('COVERX', True)) # Use cover image for indexed files (default: True)
 # If you disable it then bot will use a default thumb for all files
 
-PICS_URL = (environ.get('PICS', 'https://api.aniwallpaper.workers.dev/random?type=girl')).split() #random anime girl img each time from aniwallpaper (Experimental)
+PICS_URL = (environ.get('PICS', '')).split() #random anime girl img each time from aniwallpaper (Experimental)
 PICS = (environ.get('PICS', 'https://i.ibb.co/RktmYMqF/x.jpg https://i.ibb.co/8gN2bVsV/x.jpg https://i.ibb.co/x8qVrDdM/x.jpg https://i.ibb.co/Hf5ShsGm/x.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
