8bbbe019d2b1bd8854561d421a345a3d5cc1afbc
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2026-04-24 10:34:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+oTQk9QGQ-YRmZWI1')  # 
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002865357864")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1001648037641 -1001732234578 -1001666034664")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1001648037641")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
