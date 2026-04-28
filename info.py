0f233c190cd3c2b57bb798e7784e2933e5108b73
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2026-04-28 09:21:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002662438545')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+oTQk9QGQ-YRmZWI1')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002865357864")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1001648037641 -1001732234578 -1001666034664")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1001648037641")# Channels for force sub (make sure bot is admin)
 
 # ============================
